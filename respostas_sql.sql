16e5b8a0cc406b0c3ab323ad5698df9243f609d8
Bruno Ferreira Queles <bruno.quelestech@outlook.com>
2025-09-17 16:27:36 -0300

    Adiciona a primeira consulta SQL

---
+++ respostas_sql.sql
@@ -0,0 +1,8 @@
+USE [Target.TesteDB];
+
+--    1. Crie uma consulta que obtenha a lista de produtos (ProductName), e a quantidade por unidade (QuantityPerUnit);
+
+SELECT
+    ProductName,
+    QuantityPerUnit
+FROM Products;
